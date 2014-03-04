e23d989d07b5a39bf5bdc17d18b1ec4f7541116f
Drew Miller <drew@drewmiller.net>
2014-03-04 00:52:13 -0500

    I am going to be SO MAD if this was it

--- config/spec-e2e.js
+++ config/spec-e2e.js
@@ -1,7 +1,7 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
   baseUrl: 'http://localhost:3000',
   specs: ['../tests/*.js'],
