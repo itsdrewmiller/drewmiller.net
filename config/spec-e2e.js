92ea37f53373cabe766fdc0e063406d776b29544
Drew Miller <dmiller@ngpvan.com>
2014-06-08 19:06:29 -0400

    switching back to address-based to see if travis likes it

--- config/spec-e2e.js
+++ config/spec-e2e.js
@@ -1,6 +1,6 @@
 exports.config = {
-  seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.0.jar',
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.0.jar',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
     'browserName': 'firefox'
   },
