8711062c067c17186ec70730afc35834461fe890
Drew Miller <dmiller@ngpvan.com>
2014-07-15 01:48:27 -0400

    updated jar version - need to come up with a less brittle way to do this

--- config/spec-e2e.js
+++ config/spec-e2e.js
@@ -1,5 +1,5 @@
 exports.config = {
-  seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.0.jar',
+  seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar',
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   capabilities: {
     'browserName': 'firefox'
